08138e5bc1db2831625d42307b7f7edf8d623437
svet177 <svetlin.andonov@adastragrp.com>
2024-07-04 18:50:43 +0300

    stdin reading

---
+++ reading_stdin_110.py
@@ -0,0 +1,10 @@
+"""read input via stdin"""
+import sys
+import re
+
+counts = {}
+for line in sys.stdin:
+    for word in re.findall(r'[a-z\']+',line.lower()):
+        counts[word] = counts.get(word, 0) + 1
+        for word, count in sorted(counts.items()):
+            print(word, count)
